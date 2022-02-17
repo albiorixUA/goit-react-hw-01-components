8f58e55ab262ae2991196122cda399a6b035d16e
albiorixUA <steblynamaxim@gmail.com>
2022-02-18 01:01:48 +0200

    add basic components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,19 @@
-export const App = () => {
+import Profile from './Profile';
+import Statistics from './Statistics';
+import user from '../data/user.json';
+import data from '../data/data.json';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
-};
+}
