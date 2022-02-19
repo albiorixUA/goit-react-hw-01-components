d8f81ed43f586629d5444e606f3e4436de464307
albiorixUA <steblynamaxim@gmail.com>
2022-02-19 13:31:11 +0200

    add

---
+++ src/components/FriendList.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import FriendListItems from './FriendListItems';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul class="friend-list">
+      {friends.map(friend => (
+        <FriendListItems
+          key={friend.id}
+          avatar={friend.avatar}
+          isOnline={friend.isOnline}
+          name={friend.name}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default FriendList;
